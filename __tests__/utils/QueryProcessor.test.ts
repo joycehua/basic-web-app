508dc7419fc5aecc1d0b79f536676e6a79bdef30
joycehua <jhua3662@Joyce>
2023-09-19 15:54:26 -0400

    Added adding functionality

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -24,4 +24,16 @@ describe("QueryProcessor", () => {
             "Joycehua"
           ));
     });
+    test('should answer name', () => {
+        const query = "name";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "Joyce"
+          ));
+    });
+
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
 });
